181ae1a10bb59164a0a01554b65e129ad2d340f1
Jostein Ruen <josteinruen@gmail.com>
2024-08-15 09:10:04 +0200

    addTask tested and coded

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,24 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class TodoList {
+    HashMap<String, Boolean> toDoMap;
+
+    String output;
+
+    public TodoList(){
+        toDoMap = new HashMap<>();
+    }
+
+    public Boolean addTask(String task){
+        if (toDoMap.containsKey(task)){
+            System.out.println("Task already exists in todolist");
+            return false;
+        }else{
+            toDoMap.put(task, false);
+            return true;
+        }
+    }
 
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 class TodoListTest {
     @Test
     public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+        TodoList todoList = new TodoList();
+
+        Assertions.assertTrue(todoList.addTask("Make Dinner"));
     }
+
+
 }
